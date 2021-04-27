d5ff901fb69a4e6f8c2190bd9b1d34be580538ec
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-27 11:44:16 +0530

    Logging Messages and Checking in LogCat Tab

--- app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -14,7 +14,7 @@
 // * limitations under the License.
 // */
 //
-//package com.example.android.dessertpusher
+package com.example.android.dessertpusher
 //
 //import android.os.Handler
 //import timber.log.Timber
